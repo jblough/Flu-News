cf6d8d6139e269db1c0378a1df287baa71a45e46
jblough <joe.blough@gmail.com>
2011-05-16 09:58:46 +0000

    Added progress bar to download notification.  Now downloading to the music/CDC directory.  Use MediaScannerConnection static method
    git-svn-id: file:///Users/joe/test/git/flu_svndumps/FluNews/SVNROOT@28 e5a0faef-2dc9-42db-ad92-61d735235e97

--- android_apps/trunk/FluChallenge/src/com/josephblough/fluchallenge/activities/NotificationActivity.java
+++ android_apps/trunk/FluChallenge/src/com/josephblough/fluchallenge/activities/NotificationActivity.java
@@ -1,14 +1,35 @@
 package com.josephblough.fluchallenge.activities;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NotificationActivity extends Activity {
 
+    private static final String TAG = "NotificationActivity";
+    
+    public static final String EXTRA_FILE_URL = "NotificationActivity.file";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        finish();
+	super.onCreate(savedInstanceState);
+
+	Log.d(TAG, "onCreate");
+	
+	final String fileToPlay = getIntent().getStringExtra(EXTRA_FILE_URL);
+	
+	Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+	Uri data = Uri.parse(fileToPlay/*"file:///sdcard/abc_xyz.mp3"*/);
+	intent.setDataAndType(data, "audio/mp3");
+	try {
+	    startActivity(intent);
+	} catch (ActivityNotFoundException e) {
+	    Log.e(TAG, e.getMessage(), e);
+	}
+
+	finish();
     }
 }
--- android_apps/trunk/FluChallenge/src/com/josephblough/fluchallenge/services/PodcastDownloaderService.java
+++ android_apps/trunk/FluChallenge/src/com/josephblough/fluchallenge/services/PodcastDownloaderService.java
@@ -6,16 +6,21 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.josephblough.fluchallenge.R;
+import com.josephblough.fluchallenge.activities.NotificationActivity;
+
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaScannerConnection;
 import android.media.MediaScannerConnection.MediaScannerConnectionClient;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 public class PodcastDownloaderService extends IntentService implements MediaScannerConnectionClient {
 
@@ -27,7 +32,7 @@ public class PodcastDownloaderService extends IntentService implements MediaScan
     public static final String EXTRA_TITLE = "PodcastDownloaderService.title";
     
     private MediaScannerConnection scanner = null;
-    private String fileToScan = null;
+    private String savedAudioFilename = null;
 
     public PodcastDownloaderService() {
 	super("PodcastDownloaderService");
@@ -44,44 +49,76 @@ public class PodcastDownloaderService extends IntentService implements MediaScan
 	String filename = podcastId + ".mp3";
 
 	try {
-	    File podcastDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PODCASTS);
+	    //File podcastDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PODCASTS);
+	    File podcastDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath() + File.separator + "CDC");
 	    podcastDirectory.mkdirs();
 	    File outputFile = new File(podcastDirectory, filename);
 	    //Log.d(TAG, "Saving " + url + " to " + outputFile.getAbsolutePath());
 
+	    // Create a notification to the user
+	    int notificationId = 1;
+	    try {
+		notificationId = Integer.parseInt(podcastId);
+	    }
+	    catch (NumberFormatException e) {
+	    }
+	    final Notification notification = new Notification(R.drawable.status_bar_icon, "Downloading", System.currentTimeMillis());
+	    notification.flags = notification.flags | Notification.FLAG_ONGOING_EVENT | Notification.FLAG_AUTO_CANCEL;
+	    notification.contentView = new RemoteViews(getApplicationContext().getPackageName(), R.layout.download_with_progress);
+	    Intent i = new Intent(this, NotificationActivity.class);
+	    i.putExtra(NotificationActivity.EXTRA_FILE_URL, this.savedAudioFilename);
+	    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, 0);
+	    notification.contentIntent = pendingIntent;
+	    notification.contentView.setImageViewResource(R.id.download_status_icon, R.drawable.status_bar_icon);
+	    notification.contentView.setTextViewText(R.id.download_status_text, "Downloading " + title);
+	    notification.contentView.setProgressBar(R.id.download_status_progress, 100, 0, false);
+	    final NotificationManager mgr = (NotificationManager) getApplicationContext().
+	    	getSystemService(NOTIFICATION_SERVICE);
+	    mgr.notify(notificationId, notification);
+	    
 	    URL u = new URL(url);
 	    HttpURLConnection c = (HttpURLConnection) u.openConnection();
 	    c.setRequestMethod("GET");
 	    c.setDoOutput(true);
 	    c.connect();
+	    int downloadSize = c.getContentLength();
+	    Log.d(TAG, "Download size: " + downloadSize);
 	    FileOutputStream f = new FileOutputStream(outputFile);
 	    InputStream in = c.getInputStream();
 
 	    try {
 		byte[] buffer = new byte[1024];
 		int len = 0;
+		int totalBytesRead = 0;
+		int lastPercentReported = 0;
 		while ( (len = in.read(buffer)) != -1 ) {
 		    f.write(buffer,0, len);
+		    totalBytesRead += len;
+		    
+		    int percentComplete = (int)(((float)totalBytesRead / (float)downloadSize) * 100);
+		    if (percentComplete > lastPercentReported + 4) { // Don't update for every chunk
+			lastPercentReported = percentComplete;
+			//notification.contentView.setTextViewText(R.id.download_status_text, "Downloading: " + percentComplete + "%");
+			notification.contentView.setProgressBar(R.id.download_status_progress, 100, percentComplete, false);
+			mgr.notify(notificationId, notification);
+		    }
 		}
+		
+		//notification.contentView.setTextViewText(R.id.download_status_text, "Downloading: 100%");
+		notification.contentView.setProgressBar(R.id.download_status_progress, 100, 100, false);
+		mgr.notify(notificationId, notification);
 	    }
 	    finally {
 		f.flush();
 		f.close();
 	    }
 
-	    int notificationId = 1;
-	    try {
-		notificationId = Integer.parseInt(podcastId);
-	    }
-	    catch (NumberFormatException e) {
-		
-	    }
+	    //this.savedAudioFilename = outputFile.getAbsolutePath();
+	    //this.scanner = new MediaScannerConnection(getApplication(), this);
+	    //scanner.connect();
+	    MediaScannerConnection.scanFile(getApplication(), new String[] { outputFile.getAbsolutePath() }, null, null);
 	    
-	    this.fileToScan = outputFile.getAbsolutePath();
-	    this.scanner = new MediaScannerConnection(getApplication(), this);
-	    scanner.connect();
-	    
-	    sendNotification(notificationId, title);
+	    //sendNotification(notificationId, title);
 	}
 	catch (Exception e) {
 	    Log.e(TAG, e.getMessage(), e);
@@ -93,8 +130,9 @@ public class PodcastDownloaderService extends IntentService implements MediaScan
 	Notification notification = new Notification(
 		android.R.drawable.ic_btn_speak_now, "Finished downloading",
 		System.currentTimeMillis());
-	PendingIntent notifier = PendingIntent.getActivity(this, 0, new Intent(
-		this, com.josephblough.fluchallenge.activities.NotificationActivity.class), 0);
+	Intent intent = new Intent(this, NotificationActivity.class);
+	intent.putExtra(NotificationActivity.EXTRA_FILE_URL, this.savedAudioFilename);
+	PendingIntent notifier = PendingIntent.getActivity(this, 0, intent, 0);
 	notification.setLatestEventInfo(this, "Download complete", "Downloaded "
 		+ title, notifier);
 	notification.flags = notification.flags | Notification.FLAG_AUTO_CANCEL;
@@ -102,7 +140,7 @@ public class PodcastDownloaderService extends IntentService implements MediaScan
     }
 
     public void onMediaScannerConnected() {
-	this.scanner.scanFile(this.fileToScan, "audio/mpeg");
+	this.scanner.scanFile(this.savedAudioFilename, "audio/mpeg");
     }
 
     public void onScanCompleted(String path, Uri uri) {
