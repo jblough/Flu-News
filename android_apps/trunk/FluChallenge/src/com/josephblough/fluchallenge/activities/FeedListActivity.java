bcdd1b455dc7fc3319a2a1246c7c9dab293987d0
jblough <joe.blough@gmail.com>
2011-05-18 09:46:02 +0000

    Removed search bar and now just present the keyboard when the search button is pressed.
    git-svn-id: file:///Users/joe/test/git/flu_svndumps/FluNews/SVNROOT@40 e5a0faef-2dc9-42db-ad92-61d735235e97

--- android_apps/trunk/FluChallenge/src/com/josephblough/fluchallenge/activities/FeedListActivity.java
+++ android_apps/trunk/FluChallenge/src/com/josephblough/fluchallenge/activities/FeedListActivity.java
@@ -8,31 +8,20 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewStub;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 
-public class FeedListActivity extends ListActivity implements OnItemSelectedListener, OnItemClickListener, OnEditorActionListener, TextWatcher {
+public class FeedListActivity extends ListActivity implements OnItemSelectedListener, OnItemClickListener {
 
     private final String TAG = "FeedListActivity";
     
-    private View searchView;
-    
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.feed_entry_listing);
 
 	getListView().setOnItemSelectedListener(this);
 	getListView().setOnItemClickListener(this);
@@ -62,88 +51,11 @@ public class FeedListActivity extends ListActivity implements OnItemSelectedList
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
 	if (keyCode == KeyEvent.KEYCODE_SEARCH) {
-	    return onSearchRequested();
+	    // Present the keyboard when the search key is pressed
+	    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+	    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+	    return true;
 	}
 	return super.onKeyUp(keyCode, event);
-    }
-
-    @Override
-    public boolean onSearchRequested() {
-	toggleSearch();
-
-	// Returning true indicates that we did launch the search, instead of blocking it.
-	return true;
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-	// If we're in search mode when the back button is pressed, exit search mode
-	if (keyCode == KeyEvent.KEYCODE_BACK) {
-	    if (searchView != null &&
-		    searchView.isShown()) {
-		toggleSearch();
-		return true;
-	    }
-	}
-
-	return super.onKeyDown(keyCode, event);
-    }
-
-    private void toggleSearch() {
-	if (searchView == null) {
-	    searchView = ((ViewStub) findViewById(R.id.stub_search)).inflate();
-	    EditText input = ((EditText) searchView.findViewById(R.id.input_search_query));
-	    input.setOnEditorActionListener(this);
-	    input.addTextChangedListener(this);
-	    Button searchButton = ((Button) searchView.findViewById(R.id.button_go));
-	    searchButton.setOnClickListener(new View.OnClickListener() {
-
-		public void onClick(View v) {
-		    toggleSearch();
-		}
-	    });
-
-	    searchView.setVisibility(View.VISIBLE);
-
-	    input.requestFocus();
-	    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-	    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-	}
-	else if (searchView.isShown()) {
-	    searchView.setVisibility(View.GONE);
-
-	    EditText input = ((EditText) searchView.findViewById(R.id.input_search_query));
-	    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-	    imm.hideSoftInputFromWindow(input.getWindowToken(), 0);
-	    input.clearFocus();
-	}
-	else {
-	    searchView.setVisibility(View.VISIBLE);
-
-	    EditText input = ((EditText) searchView.findViewById(R.id.input_search_query));
-	    input.requestFocus();
-	    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-	    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-	}
-    }
-
-    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-	toggleSearch();
-	v.clearFocus();
-
-	return false;
-    }
-
-    public void afterTextChanged(Editable s) {
-	getListView().setFilterText(s.toString());
-    }
-
-    public void beforeTextChanged(CharSequence s, int start, int count,
-	    int after) {
-	// TODO Auto-generated method stub
-    }
-
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-	// TODO Auto-generated method stub
     }
 }
